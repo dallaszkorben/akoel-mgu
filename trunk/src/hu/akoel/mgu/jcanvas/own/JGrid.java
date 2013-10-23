429931eea59d2b79b1374f9a89c0d2cfa65f6463
dallaszkorben@gmail.com <dallaszkorben@gmail.com@a4e65733-a376-9fc3-9856-337237ef6e91>
2013-10-23 21:33:39 +0000

    elkezdtem az origo, axis es grid parameterezeset

--- trunk/src/hu/akoel/mgu/jcanvas/own/JGrid.java
+++ trunk/src/hu/akoel/mgu/jcanvas/own/JGrid.java
@@ -1,7 +1,6 @@
 package hu.akoel.mgu.jcanvas.own;
 
 import hu.akoel.mgu.jcanvas.own.JCanvas.Level;
-
 import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Graphics2D;
@@ -21,94 +20,140 @@ public class JGrid {
 		HIGHEST
 	}
 	
-	private Level deepness;
 	private JCanvas canvas;
-//	private Color color;
-	private PainterPosition position;
-	Position deltaGrid;
+	private Type type;
+	private Color color;
+	private int widthInPixel;
+	private PainterPosition painterPosition;
+	private Position deltaGrid;
 	int crossLengthInPixel = 3;
-//	Type type;
+	PainterListener painterListener;
 
-	public JGrid( JCanvas canvas, final Type type, final Color color, final int widthInPixel, PainterPosition position, final Position deltaGrid ){
+	public JGrid( JCanvas canvas, Type type, Color color, int widthInPixel, PainterPosition painterPosition, Position deltaGrid ){
 		this.canvas = canvas;
-//		this.color = color;
-		this.position = position;
-//		this.type = type;
+		this.type = type;
+		this.color = color;
+		this.widthInPixel = widthInPixel;
+		this.painterPosition = painterPosition;
+		this.deltaGrid = deltaGrid;
 		
-		PainterListener painterListener = new PainterListener( ) {
-			
-			@Override
-			public void paintByWorldPosition(JCanvas canvas, JGraphics g2) {
-				int n, m;
-				double xStart, yStart;
-				double yPosition, xPosition;
-				double crossXLength, crossYLength;
-				
-				Size size = canvas.getWorldSize();
-				
-				xStart = (int)(size.xMin / deltaGrid.getX()) * deltaGrid.getX();
-				yStart = (int)(size.yMin / deltaGrid.getY()) * deltaGrid.getY();				
-				
-				g2.setColor( color );
-				g2.setStroke(new BasicStroke(widthInPixel));
-				
-				if(type.equals( Type.SOLID ) ){
-					m = 1;
-					xPosition = xStart;
-					while ( xPosition <= size.xMax ){
-						g2.drawLine(xPosition, size.yMin, xPosition, size.yMax);
-						xPosition = xStart + m * deltaGrid.getX(); //!!!!!!!!
-			           m++;
-			         }
-
-			         n = 1;
-			         yPosition = yStart;
-			         while ( yPosition <= size.yMax ){
-			           g2.drawLine( size.xMin, yPosition, size.xMax, yPosition );
-			           yPosition = yStart + n * deltaGrid.getY(); //!!!!!!!!
-			           n++;
-			         }
-			         
-				}else{
-					if(type.equals( Type.CROSS ) ){
-						crossXLength = crossLengthInPixel / canvas.getPixelPerUnitX();
-						crossYLength = crossLengthInPixel / canvas.getPixelPerUnitX();
-					}else{
-						crossXLength = 0;
-						crossYLength = 0;
-					}
-			        
-			    	 n = 1;
-			    	 xPosition = xStart;
-			    	 while (xPosition <= size.xMax ){
-			    		 m = 1;
-			    		 yPosition = yStart;
-			    		 while (yPosition <= size.yMax ){
-			    			 g2.drawLine( xPosition - crossXLength, yPosition, xPosition + crossXLength, yPosition );
-			    			 g2.drawLine( xPosition, yPosition - crossYLength, xPosition, yPosition + crossYLength );
-
-			    			 yPosition = yStart + m * deltaGrid.getY() ; //!!!!!!!!
-			    			 m++;
-			    		 }//while
-			    		 
-			    		 xPosition = xStart + n * deltaGrid.getX(); //!!!!!!!!
-			    		 n++;
-			    	 }//while
-				}				
-			}
-			
-			@Override
-			public void paintByViewer(JCanvas canvas, Graphics2D g2) {							
-			}
-		};
+		painterListener = new GridPainterListener( );
 		
-		if( position.equals(PainterPosition.DEEPEST ) ){
+		if( painterPosition.equals(PainterPosition.DEEPEST ) ){
 			canvas.addPainterListenerToDeepest(painterListener, Level.ABOVE);
-		}else if( position.equals( PainterPosition.MIDDLE)){
+		}else if( painterPosition.equals( PainterPosition.MIDDLE)){
 			canvas.addPainterListenerToMiddle(painterListener, Level.ABOVE);
-		}else if( position.equals(PainterPosition.HIGHEST)){
+		}else if( painterPosition.equals(PainterPosition.HIGHEST)){
 			canvas.addPainterListenerToHighest(painterListener, Level.ABOVE);
 		}
+		
+	}
+	
+	public void turnOff(){
+		if( painterPosition.equals(PainterPosition.DEEPEST ) ){
+			canvas.removePainterListenerFromDeepest(painterListener);
+		}else if( painterPosition.equals( PainterPosition.MIDDLE)){
+			canvas.removePainterListenerFromMiddle(painterListener);
+		}else if( painterPosition.equals(PainterPosition.HIGHEST)){
+			canvas.removePainterListenerFromHighest(painterListener);
+		}
+	}
+	
+	public void turnOn(){
+		if( painterPosition.equals(PainterPosition.DEEPEST ) ){
+			canvas.addPainterListenerToDeepest(painterListener, Level.ABOVE);
+		}else if( painterPosition.equals( PainterPosition.MIDDLE)){
+			canvas.addPainterListenerToMiddle(painterListener, Level.ABOVE);
+		}else if( painterPosition.equals(PainterPosition.HIGHEST)){
+			canvas.addPainterListenerToHighest(painterListener, Level.ABOVE);
+		}	
+	}
+	
+	public void setCrossLengthInPixel( int crossLengthInPixel ){
+		this.crossLengthInPixel = crossLengthInPixel;
+	}
+	
+	public void setDeltaGrid( Position deltaGrid ){
+		this.deltaGrid = deltaGrid;
+	}
+	
+	public void setWidthInPixel( int widthInPixel ){
+		this.widthInPixel = widthInPixel;
+	}
+	
+	public void setColor( Color color ){
+		this.color = color;
+	}
+	
+	public void setType( Type type ){
+		this.type = type;
+	}
+	
+	class GridPainterListener implements PainterListener{
+
+		@Override
+		public void paintByWorldPosition(JCanvas canvas, JGraphics g2) {
+			int n, m;
+			double xStart, yStart;
+			double yPosition, xPosition;
+			double crossXLength, crossYLength;
+			
+			Size size = canvas.getWorldSize();
+			
+			xStart = (int)(size.xMin / deltaGrid.getX()) * deltaGrid.getX();
+			yStart = (int)(size.yMin / deltaGrid.getY()) * deltaGrid.getY();				
+			
+			g2.setColor( color );
+			g2.setStroke(new BasicStroke(widthInPixel));
+			
+			if(type.equals( Type.SOLID ) ){
+				m = 1;
+				xPosition = xStart;
+				while ( xPosition <= size.xMax ){
+					g2.drawLine(xPosition, size.yMin, xPosition, size.yMax);
+					xPosition = xStart + m * deltaGrid.getX(); //!!!!!!!!
+		           m++;
+		         }
+
+		         n = 1;
+		         yPosition = yStart;
+		         while ( yPosition <= size.yMax ){
+		           g2.drawLine( size.xMin, yPosition, size.xMax, yPosition );
+		           yPosition = yStart + n * deltaGrid.getY(); //!!!!!!!!
+		           n++;
+		         }
+		         
+			}else{
+				if(type.equals( Type.CROSS ) ){
+					crossXLength = crossLengthInPixel / canvas.getPixelPerUnitX();
+					crossYLength = crossLengthInPixel / canvas.getPixelPerUnitX();
+				}else{
+					crossXLength = 0;
+					crossYLength = 0;
+				}
+		        
+		    	 n = 1;
+		    	 xPosition = xStart;
+		    	 while (xPosition <= size.xMax ){
+		    		 m = 1;
+		    		 yPosition = yStart;
+		    		 while (yPosition <= size.yMax ){
+		    			 g2.drawLine( xPosition - crossXLength, yPosition, xPosition + crossXLength, yPosition );
+		    			 g2.drawLine( xPosition, yPosition - crossYLength, xPosition, yPosition + crossYLength );
+
+		    			 yPosition = yStart + m * deltaGrid.getY() ; //!!!!!!!!
+		    			 m++;
+		    		 }//while
+		    		 
+		    		 xPosition = xStart + n * deltaGrid.getX(); //!!!!!!!!
+		    		 n++;
+		    	 }//while
+			}
+			
+		}
+
+		@Override
+		public void paintByViewer(JCanvas canvas, Graphics2D g2) {	}
 		
 	}
 	
--- trunk/src/hu/akoel/mgu/jcanvas/own/JOrigo.java
+++ trunk/src/hu/akoel/mgu/jcanvas/own/JOrigo.java
@@ -15,43 +15,76 @@ public class JOrigo {
 		HIGHEST
 	}
 	
-	private Level deepness;
 	private JCanvas canvas;
-//	private Color color;
-	private PainterPosition position;
-//	Type type;
+	private Color color;
+	private int widthInPixel;
+	private double length;
+	private PainterPosition painterPosition;
+	private PainterListener painterListener;
 
-	public JOrigo( JCanvas canvas, final Color color, final int widthInPixel, final double length, PainterPosition position ){
+	public JOrigo( JCanvas canvas, Color color, int widthInPixel, double length, PainterPosition painterPosition ){
 		this.canvas = canvas;
-//		this.color = color;
-		this.position = position;
-//		this.type = type;
+		this.color = color;
+		this.length = length;  
+		this.widthInPixel = widthInPixel;
+		this.painterPosition = painterPosition;
 		
-		PainterListener painterListener = new PainterListener( ) {
-			
-			@Override
-			public void paintByWorldPosition(JCanvas canvas, JGraphics g2) {
-				
-				g2.setColor( color );
-				g2.setStroke(new BasicStroke(widthInPixel));
-				
-				g2.drawLine( (0 - length/2.0), 0, (0 + length/2.0), 0 );
-				g2.drawLine( 0, (0 - length/2.0), 0, (0 + length/2.0) );
-			}
-			
-			@Override
-			public void paintByViewer(JCanvas canvas, Graphics2D g2) {							
-			}
-		};
+		painterListener = new OrigoPainterListener( );
 		
-		if( position.equals(PainterPosition.DEEPEST ) ){
+		if( painterPosition.equals(PainterPosition.DEEPEST ) ){
 			canvas.addPainterListenerToDeepest(painterListener, Level.ABOVE);
-		}else if( position.equals( PainterPosition.MIDDLE)){
+		}else if( painterPosition.equals( PainterPosition.MIDDLE)){
 			canvas.addPainterListenerToMiddle(painterListener, Level.ABOVE);
-		}else if( position.equals(PainterPosition.HIGHEST)){
+		}else if( painterPosition.equals(PainterPosition.HIGHEST)){
 			canvas.addPainterListenerToHighest(painterListener, Level.ABOVE);
 		}
-		
+	}
+	
+	public void turnOff(){
+		if( painterPosition.equals(PainterPosition.DEEPEST ) ){
+			canvas.removePainterListenerFromDeepest(painterListener);
+		}else if( painterPosition.equals( PainterPosition.MIDDLE)){
+			canvas.removePainterListenerFromMiddle(painterListener);
+		}else if( painterPosition.equals(PainterPosition.HIGHEST)){
+			canvas.removePainterListenerFromHighest(painterListener);
+		}
+	}
+	
+	public void turnOn(){
+		if( painterPosition.equals(PainterPosition.DEEPEST ) ){
+			canvas.addPainterListenerToDeepest(painterListener, Level.ABOVE);
+		}else if( painterPosition.equals( PainterPosition.MIDDLE)){
+			canvas.addPainterListenerToMiddle(painterListener, Level.ABOVE);
+		}else if( painterPosition.equals(PainterPosition.HIGHEST)){
+			canvas.addPainterListenerToHighest(painterListener, Level.ABOVE);
+		}	
+	}
+	
+	public void setColor( Color color ){
+		this.color = color;
+	}
+	
+	public void setLength( double length ){
+		this.length = length;
+	}
+	
+	public void setWidthInPixel( int widthInPixel ){
+		this.widthInPixel = widthInPixel;
+	}
+	
+	class OrigoPainterListener implements PainterListener{
+
+		@Override
+		public void paintByWorldPosition(JCanvas canvas, JGraphics g2) {
+			g2.setColor( color );
+			g2.setStroke(new BasicStroke(widthInPixel));
+			
+			g2.drawLine( (0 - length/2.0), 0, (0 + length/2.0), 0 );
+			g2.drawLine( 0, (0 - length/2.0), 0, (0 + length/2.0) );
+		}
+
+		@Override
+		public void paintByViewer(JCanvas canvas, Graphics2D g2) {}
 	}
 	
 }
