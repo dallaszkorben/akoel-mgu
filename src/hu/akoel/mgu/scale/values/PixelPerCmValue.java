35b82c18bf197149229fd1a71940645d82579543
dallaszkorben@gmail.com <dallaszkorben@gmail.com@a4e65733-a376-9fc3-9856-337237ef6e91>
2013-10-29 11:54:46 +0000

    Elvileg rendbe rakva csomagok szintjen 2

---
+++ src/hu/akoel/mgu/scale/values/PixelPerCmValue.java
@@ -0,0 +1,11 @@
+package hu.akoel.mgu.scale.values;
+
+import hu.akoel.mgu.values.Value2D;
+
+public class PixelPerCmValue extends Value2D{
+
+	public PixelPerCmValue(double x, double y) {
+		super(x, y);
+	}
+
+}
---
+++ src/hu/akoel/mgu/scale/values/ScaleValue.java
@@ -0,0 +1,11 @@
+package hu.akoel.mgu.scale.values;
+
+import hu.akoel.mgu.values.Value2D;
+
+public class ScaleValue extends Value2D{
+
+	public ScaleValue(double x, double y) {
+		super(x, y);
+	}
+
+}
