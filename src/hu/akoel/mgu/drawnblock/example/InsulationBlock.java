787fe0a6573cc091ec78d95a41eed73ee3cf5861
dallaszkorben@gmail.com <dallaszkorben@gmail.com@a4e65733-a376-9fc3-9856-337237ef6e91>
2014-01-30 00:10:20 +0000

    0 vastag elem is rajzolhato maximalhato a DrawnBlock merete

--- src/hu/akoel/mgu/drawnblock/example/InsulationBlock.java
+++ src/hu/akoel/mgu/drawnblock/example/InsulationBlock.java
@@ -5,107 +5,48 @@ import java.awt.Color;
 import java.awt.Stroke;
 
 import hu.akoel.mgu.drawnblock.DrawnBlock;
+import hu.akoel.mgu.drawnblock.DrawnBlock.Status;
 
 public class InsulationBlock extends DrawnBlock{
+	
+	private static final long serialVersionUID = -1892355718504744390L;
+	
+	private static final Color NORMAL_COLOR = Color.green;
+	private static final Stroke NORMAL_STROKE = new BasicStroke(3);
+	private static final Color NORMAL_BACKGROUND = new Color( 0, 100, 0 );
+	
+	private static final Color SELECTED_COLOR = Color.cyan;
+	private static final Stroke SELECTED_STROKE = new BasicStroke(3);
+	private static final Color SELECTED_BACKGROUND = new Color( 0, 100, 0 );
+	
+	private static final Color INFOCUS_COLOR = Color.magenta;
+	private static final Stroke INFOCUS_STROKE = new BasicStroke(1);
+	private static final Color INFOCUS_BACKGROUND = new Color( 0, 0, 100 );
+	
+	private static final Color INPROCESS_COLOR = Color.green;
+	private static final Stroke INPROCESS_STROKE = new BasicStroke(3);
+	private static final Color INPROCESS_BACKGROUND = Color.black;
+	
+	private InsulationBlock(Status status, double x1, double y1) {
+		super(status, x1, y1);
 		
-	private Color normalColor = Color.green;
-	private Stroke normalStroke = new BasicStroke(1);
-	private Color normalBackgroundColor = new Color( 0, 100, 0 );
-	
-	private Color selectedColor = Color.cyan;
-	private Stroke selectedStroke = new BasicStroke(3);
-	private Color selectedBackgroundColor = Color.black;
-	
-	private Color infocusColor = Color.magenta;
-	private Stroke infocusStroke = new BasicStroke(1);
-	private Color infocusBackgroundColor = Color.black;
-	
-	private Color inprocessColor = Color.red;
-	private Stroke inprocessStroke = new BasicStroke(3);
-	private Color inprocessBackgroundColor = Color.black;
-	
-	public InsulationBlock(Status status, double x1, double y1, double x2, double y2) {
-		super(status, x1, y1, x2, y2);
-		setStatus(status);
-	}
-
-	public Color getNormalColor() {
-		return normalColor;
-	}
-
-	public Stroke getNormalStroke() {
-		return normalStroke;
-	}
-
-	public Color getNormalBackgroundColor() {
-		return normalBackgroundColor;
-	}
-
-	public Color getSelectedColor() {
-		return selectedColor;
-	}
-
-	public Stroke getSelectedStroke() {
-		return selectedStroke;
-	}
-
-	public Color getSelectedBackgroundColor() {
-		return selectedBackgroundColor;
-	}
-
-	public Color getInfocusColor() {
-		return infocusColor;
-	}
-
-	public Stroke getInfocusStroke() {
-		return infocusStroke;
-	}
-
-	public Color getInfocusBackgroundColor() {
-		return infocusBackgroundColor;
-	}
-
-	public Color getInprocessColor() {
-		return inprocessColor;
-	}
-
-	public Stroke getInprocessStroke() {
-		return inprocessStroke;
-	}
-
-	public Color getInprocessBackgroundColor() {
-		return inprocessBackgroundColor;
-	}
-	
-	public void setNormal( Color color, Stroke stroke, Color backgroundColor ){
-		this.normalColor = color;
-		this.normalStroke = stroke;
-		this.normalBackgroundColor = backgroundColor;
+		setNormal( NORMAL_COLOR, NORMAL_STROKE, NORMAL_BACKGROUND );
+		setSelected( SELECTED_COLOR, SELECTED_STROKE, SELECTED_BACKGROUND );
+		setInfocus(INFOCUS_COLOR, INFOCUS_STROKE, INFOCUS_BACKGROUND );
+		setInprocess( INPROCESS_COLOR, INPROCESS_STROKE, INPROCESS_BACKGROUND );
 		
 		refreshStatus();
 	}
 
-	public void setSelected( Color color, Stroke stroke, Color backgroundColor ){
-		this.selectedColor = color;
-		this.selectedStroke = stroke;
-		this.selectedBackgroundColor = backgroundColor;
+	public InsulationBlock( Status status, double x1, double y1, java.lang.Double minLength, java.lang.Double maxLength, java.lang.Double minWidth, java.lang.Double maxWidth ) {
+		super(status, x1, y1, minLength, maxLength, minWidth, maxWidth );
 		
-		refreshStatus();
-	}
-	
-	public void setInfocus( Color color, Stroke stroke, Color backgroundColor ){
-		this.infocusColor = color;
-		this.infocusStroke = stroke;
-		this.infocusBackgroundColor = backgroundColor;
+		setNormal( NORMAL_COLOR, NORMAL_STROKE, NORMAL_BACKGROUND );
+		setSelected( SELECTED_COLOR, SELECTED_STROKE, SELECTED_BACKGROUND );
+		setInfocus(INFOCUS_COLOR, INFOCUS_STROKE, INFOCUS_BACKGROUND );
+		setInprocess( INPROCESS_COLOR, INPROCESS_STROKE, INPROCESS_BACKGROUND );
 		
 		refreshStatus();
 	}
 
-	public void setInprocess( Color color, Stroke stroke, Color backgroundColor ){
-		this.inprocessColor = color;
-		this.inprocessStroke = stroke;
-		this.inprocessBackgroundColor = backgroundColor;
-		
-		refreshStatus();
-	}
 }
